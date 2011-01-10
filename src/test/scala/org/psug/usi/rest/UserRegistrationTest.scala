58d336ccf9c99678c521e522088d45e161590248
Arnaud Bailly <abailly@murex.com>
2011-01-10 22:28:44 +0100

    Bump lift-json version and make test use true json data

--- src/test/scala/org/psug/usi/rest/UserRegistrationTest.scala
+++ src/test/scala/org/psug/usi/rest/UserRegistrationTest.scala
@@ -2,19 +2,24 @@ package org.psug.usi.rest
 
 import org.junit.Assert._
 import org.junit.Test
-import org.junit.Before
-import org.junit.After
 
 import org.hamcrest.CoreMatchers._
 import org.junit.matchers.JUnitMatchers._
+
+import net.liftweb.json._
+import JsonDSL._
+import JsonAST._
 
 class UserRegistrationTest extends RESTTestUtilities {
 
   @Test
   def succeedsIfUserDoesNotExist() = {
-    def userDescription = "{\"firstname\": \"Martin\", \"lastname\": \"Odersky\", \"mail\": \"m.odersky@scala-lang.org\", \"password\": \"0xcafebabe\"}"
-    def response = webResource.path("/api/user/").header("Content-Type","application/json").post(classOf[String], userDescription)
+    val userDescription = compact(render(("firstname"	-> "Martin") ~ 
+					 ("lastname"	-> "Odersky") ~ 
+					 ("mail"	-> "m.odersky@scala-lang.org") ~ 
+					 ("password"	-> "0xcafebabe")))
+    val response = webResource.path("/api/user/").header("Content-Type","application/json").post(classOf[String], userDescription)
     assertThat(response, is("1"))
   }
- 
+  
 }
