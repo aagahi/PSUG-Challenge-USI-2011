0b34537f11316c8df62ebc7e5d3dea6aa0361a3f
Arnaud Bailly <arnaud.oqube@gmail.com>
2011-02-04 18:01:32 +0100

    added scala script for starting web app

---
+++ usi.scala
@@ -0,0 +1,70 @@
+/**
+ * Script de lancement du serveur USI 2011 
+ *  - mvn est dans le PATH
+ *  - le repertoire courant est la racine du projet challenge-usi
+ */
+
+import java.io._
+import scala.xml.pull._
+import scala.io.Source._
+import Stream._
+import scala.xml.XML._
+import System._
+
+object usi { 
+
+  val EOL = getProperty("line.separator")
+  val PATH = getProperty("path.separator")
+  val cpFile = "target/classpath.txt"
+
+  def system (cmd: String*): Process = {
+    new ProcessBuilder (cmd:_*).redirectErrorStream(true).start
+  }
+
+  def system (cmd: Array[String], env : Map[String,String]): Process = {
+    val pb = new ProcessBuilder (cmd:_*).redirectErrorStream(true)
+    for((k,v) <- env) pb.environment.put(k,v)
+    pb.start
+  }
+
+  def assertInRootDirectory : Boolean = { 
+    val cwd = new File(System.getProperty("user.dir"))
+    try { 
+      (loadFile(new File(cwd,"pom.xml")) \ "artifactId").text == "challenge-usi"
+    } catch { 
+      case e => false
+    }
+  }
+
+  def collectOutput(proc : Process) : Unit =   
+    new Thread {  override def run() = for(c <- fromInputStream(proc.getInputStream)) print(c) }.start
+
+  def reap(proc : Process) : Unit = 
+    Runtime.getRuntime().addShutdownHook(new Thread { override def run() = proc.destroy })
+
+  def main(args: Array[String]) = { 
+
+    if(!assertInRootDirectory) { 
+      println("Not in toplevel directory for project challenge-usi, giving up")
+      exit(1)
+    }
+    
+    val pb = system("mvn.bat","-q","dependency:build-classpath","-Dmdep.outputFile=" + cpFile)
+
+    if(pb.waitFor != 0) { 
+      println("Failed to generate classpath.txt. Is maven in your PATH?")
+      exit(1)
+    } 
+
+    val classpath = "target/classes" + PATH + fromFile(new File(cpFile)).mkString
+
+    val java = system(Array("java","org.psug.usi.Main"), Map("CLASSPATH" -> classpath))
+    
+    collectOutput(java)
+    reap(java)
+    java.waitFor
+  }
+}
+
+
+usi.main(args)
