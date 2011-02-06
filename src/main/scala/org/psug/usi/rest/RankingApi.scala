a89061a93d14872b4046e812ce5aa1aae9663d2a
Francois <francois.armand@normation.com>
2011-02-06 23:04:23 +0100

    Add missing rest API classes - without implementation; add spec in comments

---
+++ src/main/scala/org/psug/usi/rest/RankingApi.scala
@@ -0,0 +1,39 @@
+package org.psug.usi.rest
+
+/*
+ * Obtenir le classement d'un utilisateur
+ *
+ *  * URI : .../api/ranking
+ *  * méthode GET
+ *  * Paramètre de retour 
+ *
+ *  {
+ *    "top_scores" : {
+ *      "mail" : [ "string", "string", ...],
+ *      "scores" : [ number, number, ...]
+ *    }
+ *    "before_me" : {
+ *      "mail" : ["string", "string", ...],
+ *      "scores" : [number, number, ...]
+ *    }
+ *    "after_me" : {
+ *      "mail" : ["string", "string", ...],
+ *      "scores" : [number, number, ...]
+ *    }
+ *  }
+ *
+ *  * Codes de retour
+ *        o OK 200
+ *        o Clé de session non reconnue : 401
+ *        o Autre : 400
+ *  * Commentaires
+ *        o Retourne les 100 premiers scores (couple mail/score) et les 100 scores avoisinant 
+ *          l'utilisateur (50 avant et 50 après). Si l'utilisateur courant est classé 40ème, 
+ *          (donc dans le 50 premiers) seuls les 39 utilisateurs le précédant et les 
+ *          50 suivants seront retournés.
+ *        o L'authentification de l'utilisateur se fait par le cookie 'session_key'.
+ */
+
+class RankingApi {
+  //TODO
+}
