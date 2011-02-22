74016419583aa0899f5d8864916046aa59dba6ec
Alexis Agahi <alag@users.sf.net>
2011-02-22 02:08:36 +0100

    J'ai ajouté une implem experimental de Netty. Il manque les test :/ => pas encore compris comment fonctionnait jersey en mode client pour les test ;)

---
+++ src/main/scala/org/psug/usi/netty/HttpRequestHandler.scala
@@ -0,0 +1,71 @@
+package org.psug.usi.netty
+
+import org.psug.usi.users.User
+import org.jboss.netty.util.CharsetUtil
+import net.liftweb.json.{Serialization, NoTypeHints}
+import org.jboss.netty.channel._
+import org.jboss.netty.buffer.ChannelBuffers
+import org.jboss.netty.handler.codec.http._
+import net.liftweb.json.Serialization.{read, write}
+
+/**
+ * User: alag
+ * Date: 2/22/11
+ * Time: 1:07 AM
+ */
+
+
+@ChannelHandler.Sharable
+class HttpRequestHandler extends SimpleChannelUpstreamHandler {
+  implicit val formats = Serialization.formats(NoTypeHints)
+
+  override def messageReceived( ctx:ChannelHandlerContext , event:MessageEvent ) {
+    val channel = ctx.getChannel
+
+    val response = new DefaultHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK)
+    response.setHeader(HttpHeaders.Names.CONTENT_TYPE, "text/plain; charset=UTF-8")
+
+    event.getMessage match
+    {
+      case request:HttpRequest => handleRequest( request, response )
+      case _ => println( "Unknown message" )
+    }
+
+    event.getChannel().write(response);
+
+  }
+
+  private def handleRequest( request:HttpRequest, response:HttpResponse ){
+
+    val method = request.getMethod
+    val queryStringDecoder = new QueryStringDecoder( request.getUri() )
+    val content = request.getContent().toString(CharsetUtil.UTF_8)
+
+    ( method, queryStringDecoder.getPath.split('/').tail ) match {
+
+      case ( HttpMethod.GET, Array("api","user",userId) )  =>
+        val str = write(User(0,"Martin", "Odersky","m.odersky@scala-lang.org","0xcafebabe"))
+        response.setContent(ChannelBuffers.copiedBuffer( str, CharsetUtil.UTF_8))
+
+        
+      case ( HttpMethod.POST, Array("api","user") ) =>
+        val user = read[User](content)
+        if(user.firstName == "Myriam")
+          response.setStatus(HttpResponseStatus.BAD_REQUEST)
+        else
+          response.setContent(ChannelBuffers.copiedBuffer( "1", CharsetUtil.UTF_8))
+
+
+      case _ =>
+    }
+
+
+
+  }
+
+
+
+  override def exceptionCaught( ctx:ChannelHandlerContext, e:ExceptionEvent ){
+      e.getChannel.close();
+  }
+}
---
+++ src/main/scala/org/psug/usi/netty/WebServer.scala
@@ -0,0 +1,61 @@
+package org.psug.usi.netty
+
+import org.jboss.netty.bootstrap.ServerBootstrap
+import org.jboss.netty.channel.socket.nio.NioServerSocketChannelFactory
+
+import java.net.InetSocketAddress
+import org.jboss.netty.channel._
+import org.jboss.netty.handler.codec.http.{HttpResponseEncoder, HttpRequestDecoder}
+import java.util.concurrent.Executors
+
+
+/**
+ * User: alag
+ * Date: 2/22/11
+ * Time: 12:46 AM
+ */
+
+object WebServer{
+  lazy val defaultWebServer = new WebServer
+
+  val ioBufferSize = 1024*8
+  val keepAlive = true
+  val backLog = 5000
+  val tcpNoDelay = true
+}
+
+
+class WebServer( val listenPort:Int = 18080 )  {
+  import  WebServer._
+  val bootstrap = new ServerBootstrap(
+                                    new NioServerSocketChannelFactory(
+                                    Executors.newCachedThreadPool(),
+                                    Executors.newCachedThreadPool()))
+
+
+  bootstrap.setOption("child.tcpNoDelay", tcpNoDelay );
+  bootstrap.setOption("child.receiveBufferSize", ioBufferSize )
+  bootstrap.setOption("child.sendBufferSize", ioBufferSize )
+  bootstrap.setOption("child.keepAlive", keepAlive )
+  bootstrap.setOption( "backlog", backLog )
+
+
+  bootstrap.setPipelineFactory( new HttpServerPipelineFactory() )
+  bootstrap.bind( new InetSocketAddress( "0.0.0.0", listenPort ) )
+
+  println("Web Initializer bound to 0.0.0.0:"+listenPort+": "+ bootstrap.getOptions )
+}
+
+
+
+class HttpServerPipelineFactory extends ChannelPipelineFactory {
+
+    def getPipeline() = {
+        val pipeline = Channels.pipeline()
+        pipeline.addLast("decoder", new HttpRequestDecoder());
+        pipeline.addLast("encoder", new HttpResponseEncoder());
+        pipeline.addLast("handler", new HttpRequestHandler())
+        pipeline
+      }
+}
+
