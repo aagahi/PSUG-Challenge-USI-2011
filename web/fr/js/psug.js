9271760490c296d706b9117d8011ac11b0e3370f
Gabriel Kastenbaum <gabriel.kastenbaum@gmail.com>
2011-04-11 02:29:44 +0200

    bugfix. The email entered (when creating a user) is checked

--- web/fr/js/psug.js
+++ web/fr/js/psug.js
@@ -12,3 +12,9 @@ var usi_log=function(msg){
     var $dbg=$("#usi_debug");
     $dbg.append(msg+"<br />");
 };
+
+var validateEmail=function(email) 
+{ 
+ var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\ ".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA -Z\-0-9]+\.)+[a-zA-Z]{2,}))$/ 
+ return email.match(re) 
+}
